68adc7504bacf9589bbc9c66451ceca47676c93d
Guilherme Vinícius <guilherme.vinicius.s.santos@gmail.com>
2024-09-05 18:14:20 -0300

    novas imagens

--- js/dados.js
+++ js/dados.js
@@ -2,27 +2,42 @@ const deusesGodOfWar = [
   {
     nome: 'Kratos',
     titulo: 'Deus da Guerra',
-    descricao: 'O Fantasma de Esparta, também ex-general espartano matou sua família após ser enganado por Ares. Vingou-se do deus, tornando-se o novo deus da guerra. Buscando vingança contra todos os deuses do Olimpo, Kratos se torna o protagonista da série.'
+    descricao: 'O Fantasma de Esparta, ourtrora ex-general espartano. Matou sua família após ser enganado por Ares, o deus da guerra. Em uma tentativa desesperada de se livrar de suas memórias, Kratos se agarraria ao pequeno vislumbre de esperança de que talvez um dia fosse capaz de se redimir e realizou trabalhos para os Deuses até conseguir sua vingança, tornando- se o novo deus da guerra. Buscando a queda dos deuses do Olimpo que o enganaram, Kratos se torna o protagonista da série.'
+  },
+  {
+    nome: 'Zeus',
+    titulo: 'Rei dos deuses do Olimpo',
+    descricao: 'Zeus, o rei do Olimpo e deus do céu, derrotou seu pai Cronos para liderar os deuses do Panteão Grego. Com ajuda de seus irmãos, Hades e Poseidon, ele venceu a guerra contra os Titãs, banindo-os ao Tártaro. Criou a Caixa de Pandora para conter os males liberados pela guerra e construiu o Monte Olimpo. Zeus teve muitos filhos, incluindo deuses como Atena e Ares, ele manipulou Kratos para seus fins, mas o traiu, levando o espartano a buscar vingança contra o pai.'
+  },
+  {
+    nome: 'Poseidon',
+    titulo: 'Deus dos Mares',
+    descricao: 'Poseidon, deus dos mares, foi engolido por Cronos na infância, sendo libertado por Zeus. Participou ativamente da guerra contra os Titãs e tornou-se uma das divindades mais poderosas, subordinado apenas a Zeus. Em God of War, Poseidon auxilia Kratos, mas mais tarde se torna seu antagonista, tentando proteger o Olimpo. Após uma intensa batalha, Kratos o mata brutalmente, causando uma inundação catastrófica que devastou o mundo grego e levou à morte de muitos mortais.'
+  },
+  {
+    nome: 'Hades',
+    titulo: 'Deus do Mundo dos Mortos',
+    descricao: 'Hades era o deus do mundo dos mortos, da riqueza, e dos mortos. Ele é irmão de Zeus e Poseidon. Filho de Cronos e Réia. Ele foi anteriormente um aliado do Kratos na luta contra Ares, mas devido a morte de sua esposa e outros membros de sua família pelas mãos do Kratos, ele se torna um dos principais antagonistas em God of War III. Possui um elmo; seu corpo é vermelho e coberto por alguns espinhos. Hades é o segundo Deus mais poderoso, superado apenas por seu irmão, Zeus.',
   },
   {
     nome: 'Atena',
     titulo: 'Deusa da Sabedoria e da Guerra',
-    descricao: 'Mentora e aliada de Kratos. Atena dá a Kratos a tarefa de matar Ares sendo isso fundamental para fazer de Kratos o Deus da Guerra. Embora tendo mentido para Kratos sobre seu irmão Deimos, Atena sempre se fez solidária a Kratos, mesmo quando este renunciou aos deuses e foi traído por Zeus.'
+    descricao: 'Mentora e aliada de Kratos. Atena dá a Kratos a tarefa de matar Ares sendo isso fundamental para fazer de Kratos o deus da Guerra. Embora tendo mentido para Kratos sobre seu irmão Deimos, Atena sempre se fez solidária a Kratos, mesmo quando este renunciou aos deuses e foi traído por Zeus.'
   },
   {
     nome: 'Ares',
     titulo: 'Deus da Guerra',
-    descricao: 'O principal antagonista do primeiro God of War, Ares manipula Kratos para que se torne um instrumento de guerra. É o responsável pela morte da família de Kratos e por suas Lâminas do Caos.'
+    descricao: 'O principal antagonista do primeiro God of War, foi o deus da Guerra olímpico original, filho de Zeus e Hera, foi responsável por colocar os eventos da série em movimento, enganando Kratos para que matasse sua família e o transformasse no Fantasma de Esparta, o que eventualmente cumpriu a profecia do Guerreiro Marcado que destruiria o Olimpo e acabaria com o reinado dos Deuses.'
   },
   {
-    nome: 'Zeus',
-    titulo: 'Rei dos Deuses',
-    descricao: 'O poderoso líder do Olimpo, Zeus é o pai de muitos deuses e heróis. Representa o poder absoluto e a corrupção que podem surgir quando este é mal utilizado. Manipulador e ambicioso, ele utiliza Kratos para seus próprios fins, mas acaba traindo seu filho levando o espartano a buscar vingança.'
+    nome: 'Gaia',
+    titulo: 'Titã da Terra',
+    descricao: 'Tendo sua primeira aparição em God of War II, a mãe dos Titãs, Gaia foi banida com seus outros companheiros Titãs na conclusão da Grande Guerra. Gaia salva Kratos do Submundo após um encontro desastroso com Zeus, e pede ao espartano que ache as Irmãs do Destino para mudar seu passado.'
   },
   {
-    nome: 'Hades',
-    titulo: 'Rei do Submundo',
-    descricao: 'Deus do submundo e dos mortos, Hades governa sobre os mortos e as almas perdidas. É conhecido por sua natureza sombria e implacável.',
+    nome: 'Gaia',
+    titulo: 'Titã da Terra',
+    descricao: 'Tendo sua primeira aparição em God of War II, a mãe dos Titãs, Gaia foi banida com seus outros companheiros Titãs na conclusão da Grande Guerra. Gaia salva Kratos do Submundo após um encontro desastroso com Zeus, e pede ao espartano que ache as Irmãs do Destino para mudar seu passado.'
   },
   {
     nome: 'Gaia',
